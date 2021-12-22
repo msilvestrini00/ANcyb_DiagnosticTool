d886d62485901a626a2336a0b44886b4efbc36e3
gfiara <76902534+gfiara@users.noreply.github.com>
2021-12-22 18:48:14 +0100

    Creazione Sping Boot App

---
+++ ancyb_diagnosticTool/src/main/java/it/univpm/ancyb_diagnosticTool/AncybDiagnosticToolApplication.java
@@ -0,0 +1,13 @@
+package it.univpm.ancyb_diagnosticTool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AncybDiagnosticToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AncybDiagnosticToolApplication.class, args);
+	}
+
+}
---
+++ ancyb_diagnosticTool/src/test/java/it/univpm/ancyb_diagnosticTool/AncybDiagnosticToolApplicationTests.java
@@ -0,0 +1,13 @@
+package it.univpm.ancyb_diagnosticTool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AncybDiagnosticToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
