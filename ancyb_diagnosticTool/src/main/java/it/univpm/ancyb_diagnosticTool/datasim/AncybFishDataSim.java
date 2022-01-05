039d0604413322ed68df25b3731e92a7f99e4d55
msilvestrini00 <76901844+msilvestrini00@users.noreply.github.com>
2022-01-05 22:47:47 +0100

    Finalizzata classe 'AncybFishDataSim'
    E' da testare, utilizzandola per aggiornare il codice riguardo la prima
    rotta

--- ancyb_diagnosticTool/src/main/java/it/univpm/ancyb_diagnosticTool/datasim/AncybFishDataSim.java
+++ ancyb_diagnosticTool/src/main/java/it/univpm/ancyb_diagnosticTool/datasim/AncybFishDataSim.java
@@ -5,8 +5,9 @@ import java.util.ArrayList;
 import it.univpm.ancyb_diagnosticTool.utilities.Time;
 
 /* 
- * classe di test che simula la ricezione di un arraylist in cui sono stoccate 
- * tutte le informazioni riguardo i dispositivi (macaddress e coordinate)
+ * classe di test che simula la ricezione dei dati da MQTT con un arraylist in cui sono stoccate 
+ * tutte le informazioni riguardo i dispositivi (macaddress e coordinate).
+ * Le informazioni riguardano due dispositivi 
  */
 
 // TODO FINISCI DI IMPLEMENTARE LA CLASSE DI SIMULAZIONE, COI METODI DEL FILE WORD
@@ -23,49 +24,47 @@ public class AncybFishDataSim {
 	public AncybFishDataSim(ArrayList<AncybFishDataObjectSim> dataList) {
 		
 
-		dataList.add(new AncybFishDataObjectSim("00:00:00:00:00:00", Time.currentDateTime2()	, 11.111111, 11.111111, 1));
-		dataList.add(new AncybFishDataObjectSim("00:00:00:00:00:00", "2022-01-01T00:00:00+00:00", 22.222222, 22.222222, 2));
+		dataList.add(new AncybFishDataObjectSim("00:00:00:00:00:00", Time.currentDateTime2()	, 43.699964, 13.410088, 1));
+		dataList.add(new AncybFishDataObjectSim("00:00:00:00:00:00", "2022-01-01T00:00:00+00:00", 44.079568, 13.729647, 2));
 
-		dataList.add(new AncybFishDataObjectSim("11:11:11:11:11:11", Time.currentDateTime2()	, 33.333333, 33.333333, 3));
-		dataList.add(new AncybFishDataObjectSim("11:11:11:11:11:11", "2022-01-01T00:00:00+00:00", 44.444444, 44.444444, 4));
-
-
-
-		
-	}
-
-
-	public double getLatByMacAddr(String macAddr) {
-		
-		double lat = 0.0;
-		
-		for(AncybFishDataObjectSim dataSimObject : this.dataList) {
-			
-			if(dataSimObject.getMacAddr().equals(macAddr)) {
-				
-				lat = dataSimObject.getLat();
-			}
-			else continue;
-		}
-		
-		return lat;
+		dataList.add(new AncybFishDataObjectSim("11:11:11:11:11:11", Time.currentDateTime2()	, 42.034962, 15.436648, 3));
+		dataList.add(new AncybFishDataObjectSim("11:11:11:11:11:11", "2022-01-01T00:00:00+00:00", 41.899620, 16.289014, 4));	
 	}
 
 	
-	public double getLngByMacAddr(String macAddr) {
+
+	public AncybFishDataObjectSim getDataSim(String macAddr, String time) {
 		
-		double lng = 0.0;
+		int index = -1;
 		
-		for(AncybFishDataObjectSim dataSimObject : this.dataList) {
+		for(int i=0; i< this.dataList.size(); i++) {
 			
-			if(dataSimObject.getMacAddr().equals(macAddr)) {
+			if(dataList.get(i).getMacAddr().equals(macAddr) && dataList.get(i).getTime().equals(time)) {
 				
-				lng = dataSimObject.getLng();
+				index = dataList.indexOf(dataList.get(i));
 			}
 			else continue;
 		}
 		
-		return lng;
+		if(index == -1) {
+			
+			System.out.println("Oggetto AncybFishDataObjectSim non trovato");
+			return null;
+		}
+		
+		else return dataList.get(index);
+	}
+
+	
+	public ArrayList<AncybFishDataObjectSim> getDataSimList(String macAddr) {
+		
+		ArrayList<AncybFishDataObjectSim> list = new ArrayList<AncybFishDataObjectSim>();
+		
+		for(AncybFishDataObjectSim obj : dataList) {
+		
+			if(obj.getMacAddr().equals(macAddr)) list.add(obj);
+		}
+		return list;
 	}
 
 
