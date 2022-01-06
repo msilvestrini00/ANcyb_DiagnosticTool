47e8269d1a3a947600c722af088aee5dd5d4322c
gfiara <76902534+gfiara@users.noreply.github.com>
2022-01-06 13:24:21 +0100

    Modifiche a seguito della riunione del 6.01.2021

---
+++ ancyb_diagnosticTool/src/main/java/it/univpm/ancyb_diagnosticTool/filters/FilterInterface.java
@@ -0,0 +1,7 @@
+package it.univpm.ancyb_diagnosticTool.filters;
+
+//TODO rinonima in filter
+public interface FilterInterface {
+	public FilterInterface getDataToFilter();
+	public FilterInterface getFiltered();
+}
