4ee9644b445e8f4f0256829cb5ed565476755361
gfiara <76902534+gfiara@users.noreply.github.com>
2022-01-07 17:51:10 +0100

    Update TestANcybFishDataManaging.java

--- ancyb_diagnosticTool/src/test/java/it/univpm/ancyb_diagnosticTool/service/TestANcybFishDataManaging.java
+++ ancyb_diagnosticTool/src/test/java/it/univpm/ancyb_diagnosticTool/service/TestANcybFishDataManaging.java
@@ -28,23 +28,17 @@ class TestANcybFishDataManaging {
 	 * inserito come parametro.
 	 * @throws Exception
 	 */
+	@SuppressWarnings("unused")
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
 		
 		ANcybFishData ancybData1 = new ANcybFishData_VerGT("2022.01.06", "18:25:38", "A4:cf:12:76:76:95", "Ver_GT", 44.915f, 15.25f, "3", 10.5f);
-		ANcybFishData.list.add(ancybData1);
 		ANcybFishData ancybData2 = new ANcybFishData_VerG("2022.01.06", "18:25:52", "B4:cf:12:76:76:95", "Ver_G", 44.915f, 15.25f, "NO_signal");
-		ANcybFishData.list.add(ancybData2);
 		ANcybFishData ancybData3 = new ANcybFishData_VerGT("2022.01.06", "18:26:38", "A4:cf:12:76:76:95", "Ver_GT", 44.915f, 15.25f, "NO_signal", 10.5f);
-		ANcybFishData.list.add(ancybData3);
 		ANcybFishData ancybData4 = new ANcybFishData_VerG("2022.01.06", "18:27:38", "B4:cf:12:76:76:95", "Ver_G", 44.915f, 15.25f, "NO_signal");
-		ANcybFishData.list.add(ancybData4);
 		ANcybFishData ancybData5 = new ANcybFishData_VerGT("2022.01.06", "18:28:38", "A4:cf:12:76:76:95", "Ver_GT", 44.915f, 15.25f, "5", 10.5f);
-		ANcybFishData.list.add(ancybData5);
 		ANcybFishData ancybData6 = new ANcybFishData_VerG("2022.01.06", "18:29:38", "B4:cf:12:76:76:95", "Ver_G", 44.915f, 15.25f, "NO_signal");
-		ANcybFishData.list.add(ancybData6);
 		ANcybFishData ancybData7 = new ANcybFishData_VerGT("2022.01.06", "18:30:38", "A4:cf:12:76:76:95", "Ver_GT", 44.915f, 15.25f, "NO_signal", 10.5f);
-		ANcybFishData.list.add(ancybData7);
 		a = new AncybDiagnosticToolServiceImpl();
 		ancybDataObtainedA = a.getLastTimePosition("A4:cf:12:76:76:95");
 		ancybDataExpectedA = new ANcybFishData_VerGT("2022.01.06", "18:30:38", "A4:cf:12:76:76:95", "Ver_GT", 44.915f, 15.25f, "NO_signal", 10.5f);
