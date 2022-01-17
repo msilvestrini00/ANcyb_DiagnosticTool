0d4773a7adbf0663b4b369f1c86c92c62cdd2e47
msilvestrini00 <76901844+msilvestrini00@users.noreply.github.com>
2022-01-17 23:26:21 +0100

    Modfica import a classe 'TestMqttDataReceived'
    Modificata la riga di importazione per problemi di compatibilità

--- ancyb_diagnosticTool/src/test/java/it/univpm/ancyb_diagnosticTool/mqtt/dataReceived/test/TestMqttDataReceived.java
+++ ancyb_diagnosticTool/src/test/java/it/univpm/ancyb_diagnosticTool/mqtt/dataReceived/test/TestMqttDataReceived.java
@@ -1,6 +1,8 @@
 package it.univpm.ancyb_diagnosticTool.mqtt.dataReceived.test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+
+//import static org.junit.Assert.assertEquals;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
