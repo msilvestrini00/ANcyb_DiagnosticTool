12d1a8d3caca2139a4c224f60c26e1e64464d9de
gfiara <76902534+gfiara@users.noreply.github.com>
2022-01-18 17:16:02 +0100

    DataReceived JavaDocs

--- ancyb_diagnosticTool/src/main/java/it/univpm/ancyb_diagnosticTool/utilities/DataReceived.java
+++ ancyb_diagnosticTool/src/main/java/it/univpm/ancyb_diagnosticTool/utilities/DataReceived.java
@@ -8,10 +8,28 @@ package it.univpm.ancyb_diagnosticTool.utilities;
  */
 public interface DataReceived {
 	
+	/**
+	 *<b>Metodo</b> che restituisce la data attribuita all'oggetto. 
+	 * @return Stringa che rappresenta la data.
+	 */
 	public String getDate();
+	
+	/**
+	 * <b>Metodo</b> che imposta la data attribuita all'oggetto. 
+	 * @param date Data che si vuole impostare.
+	 */
 	public void setDate(String date);
+	
+	/**
+	 * <b>Metodo</b> che restituisce l'orario attribuito all'oggetto. 
+	 * @return Stringa che rappresenta l'orario.
+	 */
 	public String getTime();
+	
+	/**
+	 * <b>Metodo</b> che imposta l'orario attribuito all'oggetto.
+	 * @param time Orario che si vuole impostare.
+	 */
 	public void setTime(String time);
-	public String toString();
 	
 }
