7e96328fe0b85d61094455fb56285ac586c00e50
gfiara <76902534+gfiara@users.noreply.github.com>
2022-01-18 23:46:11 +0100

    JavaDocs (utilities)

--- ancyb_diagnosticTool/src/main/java/it/univpm/ancyb_diagnosticTool/utilities/CheckVersion.java
+++ ancyb_diagnosticTool/src/main/java/it/univpm/ancyb_diagnosticTool/utilities/CheckVersion.java
@@ -8,17 +8,34 @@ import it.univpm.ancyb_diagnosticTool.mqtt.dataReceived.ANcybFishData_VerG;
 import it.univpm.ancyb_diagnosticTool.mqtt.dataReceived.ANcybFishData_VerGT;
 
 /**
+ * <b>Classe</b> che effettua dei controlli sulle versioni di istanze o ArrayList di tipo
+ * {@link it.univpm.ancyb_diagnosticTool.mqtt.dataReceived.ANcybFishData ANcybFishData}.
  * 
  * @author Giacomo Fiara
- *
  */
 public class CheckVersion {
 	
+	/**
+	 * <b>Metodo</b> che controlla se la versione associata all'istanza
+	 *  corrisponde alla "Ver_G" o versioni che ereditano da questa.
+	 * @param fishData dato di cui si vuole testare la versione.
+	 * @throws VersionMismatch
+	 * 
+	 * @see it.univpm.ancyb_diagnosticTool.mqtt.dataReceived.ANcybFishData_VerG ANcybFishData_VerG
+	 */
 	public static void verG(ANcybFishData fishData) throws VersionMismatch {
 		if (!((fishData instanceof ANcybFishData_VerG)||(fishData instanceof ANcybFishData_VerGT)))
 			throw new VersionMismatch("VersionMismatch(VerG single data) --> the device isn't 'G' o 'GX' version software.");
 	}
 	
+	/**
+	 * <b>Metodo</b> che controlla se la versione associata a ciascun elemento dell'ArrayList
+	 *  corrisponde alla "Ver_G" o versioni che ereditano da questa.
+	 * @param fishDataList ArrayList contenente le istanze di cui si vogliono controllare le versioni.
+	 * @throws VersionMismatch
+	 * 
+	 * @see it.univpm.ancyb_diagnosticTool.mqtt.dataReceived.ANcybFishData_VerG ANcybFishData_VerG
+	 */
 	public static void verG(ArrayList<ANcybFishData> fishDataList) throws VersionMismatch {
 		for(ANcybFishData fishData : fishDataList) {
 			if (!((fishData instanceof ANcybFishData_VerG)||(fishData instanceof ANcybFishData_VerGT)))
@@ -27,11 +44,27 @@ public class CheckVersion {
 		}
 	}
 	
+	/**
+	 * <b>Metodo</b> che controlla se la versione associata all'istanza
+	 *  corrisponde alla "Ver_GT" o versioni che ereditano da questa.
+	 * @param fishData dato di cui si vuole testare la versione.
+	 * @throws VersionMismatch
+	 * 
+	 * @see it.univpm.ancyb_diagnosticTool.mqtt.dataReceived.ANcybFishData_VerGT ANcybFishData_VerGT
+	 */
 	public static void verGT(ANcybFishData fishData) throws VersionMismatch {
 		if (!(fishData instanceof ANcybFishData_VerGT))
 			throw new VersionMismatch("VersionMismatch(VerGT single data) --> the device isn't 'GT' version software.");
 	}
 	
+	/**
+	 * <b>Metodo</b> che controlla se la versione associata a ciascun elemento dell'ArrayList
+	 *  corrisponde alla "Ver_GT" o versioni che ereditano da questa.
+	 * @param fishDataList ArrayList contenente le istanze di cui si vogliono controllare le versioni.
+	 * @throws VersionMismatch
+	 * 
+	 * @see it.univpm.ancyb_diagnosticTool.mqtt.dataReceived.ANcybFishData_VerGT ANcybFishData_VerGT
+	 */
 	public static void verGT(ArrayList<ANcybFishData> fishDataList) throws VersionMismatch {
 		for(ANcybFishData fishData : fishDataList) {
 			if (!(fishData instanceof ANcybFishData_VerGT))
--- ancyb_diagnosticTool/src/main/java/it/univpm/ancyb_diagnosticTool/utilities/IsVersion.java
+++ ancyb_diagnosticTool/src/main/java/it/univpm/ancyb_diagnosticTool/utilities/IsVersion.java
@@ -6,8 +6,24 @@ import it.univpm.ancyb_diagnosticTool.mqtt.dataReceived.ANcybFishData;
 import it.univpm.ancyb_diagnosticTool.mqtt.dataReceived.ANcybFishData_VerG;
 import it.univpm.ancyb_diagnosticTool.mqtt.dataReceived.ANcybFishData_VerGT;
 
+/**
+ * <b>Classe</b> che effettua dei controlli sulle versioni di ArrayList di tipo
+ * {@link it.univpm.ancyb_diagnosticTool.mqtt.dataReceived.ANcybFishData ANcybFishData}.
+ * 
+ * @author Giacomo Fiara
+ */
 public class IsVersion {
 	
+	/**
+	 * <b>Metodo</b> che controlla se la versione associata a ciascun elemento dell'ArrayList
+	 *  corrisponde alla "Ver_G" o versioni che ereditano da questa.
+	 * @param fishDataList ArrayList contenente le istanze di cui si vogliono controllare le versioni.
+	 * @return booleano:<br>
+	 * - Falso se almeno un elemento presenta una versione diversa dalla "Ver_G". <br>
+	 * - Vero se tutti gli elementi sono di versione "Ver_G".
+	 * 
+	 * @see it.univpm.ancyb_diagnosticTool.mqtt.dataReceived.ANcybFishData_VerG ANcybFishData_VerG
+	 */
 	public static boolean verG(ArrayList<ANcybFishData> fishDataList)  {
 		for(ANcybFishData fishData : fishDataList) {
 			if (!((fishData instanceof ANcybFishData_VerG)||(fishData instanceof ANcybFishData_VerG)))
@@ -16,6 +32,16 @@ public class IsVersion {
 		return true;
 	}
 	
+	/**
+	 * <b>Metodo</b> che controlla se la versione associata a ciascun elemento dell'ArrayList
+	 *  corrisponde alla "Ver_GT" o versioni che ereditano da questa.
+	 * @param fishDataList ArrayList contenente le istanze di cui si vogliono controllare le versioni.
+	 * @return booleano:<br>
+	 * - Falso se almeno un elemento presenta una versione diversa dalla "Ver_GT". <br>
+	 * - Vero se tutti gli elementi sono di versione "Ver_GT".
+	 * 
+	 * @see it.univpm.ancyb_diagnosticTool.mqtt.dataReceived.ANcybFishData_VerG ANcybFishData_VerG
+	 */
 	public static boolean verGT(ArrayList<ANcybFishData> fishDataList) {
 		for(ANcybFishData fishData : fishDataList) {
 			if (!((fishData instanceof ANcybFishData_VerGT)||(fishData instanceof ANcybFishData_VerGT)))
